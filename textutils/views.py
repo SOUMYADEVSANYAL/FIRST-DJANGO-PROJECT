4ca4ec69f40ae6c6eb91d4128ae5d823efcf7af1
Soumyadev Sanyal <soumya.sp35@gmail.com>
2020-01-01 11:48:27 +0530

    Multiple operation unlocked

--- textutils/views.py
+++ textutils/views.py
@@ -8,8 +8,14 @@ def index(request):
 
 def analyze(request):
     textvar =  request.POST.get('text','No Text')
-    ana = request.POST.get('ana', '0')
-    if (ana == "1"):
+    ana1 = request.POST.get('ana1', '0')
+    ana2 = request.POST.get('ana2', '0')
+    ana3 = request.POST.get('ana3', '0')
+    ana4 = request.POST.get('ana4', '0')
+    ana5 = request.POST.get('ana5', '0')
+    ana6 = request.POST.get('ana6', '0')
+    counted="Not counted"
+    if (ana1 == "1"):
         punctuations = '''!()-[]{};:'"\\,<>./?@#$%^&*_~'''
         analyzed = ""
         for char in textvar:
@@ -17,46 +23,55 @@ def analyze(request):
                 analyzed = analyzed+"\n"
             if char not in punctuations:
                 analyzed = analyzed + char
-        params = {'purpose':'After removing punctuations', 'result': analyzed}
-        return render(request, 'analyze.html', params)
+        textvar=analyzed
+        # params = {'purpose':'After removing punctuations', 'result': analyzed}
+        # return render(request, 'analyze.html', params)
 
-    elif(ana == "2"):
+    if(ana2 == "1"):
         analyzed = ''
         for char in textvar:
             if (char == ' '):
                 continue
             analyzed=analyzed+char
-        params = {'purpose':'After removing spaces', 'result': analyzed}
-        return render(request, 'analyze.html', params)
+        textvar=analyzed
+        # params = {'purpose':'After removing spaces', 'result': analyzed}
+        # return render(request, 'analyze.html', params)
 
-    elif(ana == "3"):
+    if(ana3 == "1"):
         length=len(textvar)
-        params = {'purpose':'Number of characters', 'result': length}
-        return render(request,'analyze.html',params)
+        counted= str(length)
+        # params = {'purpose':'Number of characters', 'result': length}
+        # return render(request,'analyze.html',params)
     
-    elif(ana == "4"):
+    if(ana4 == "1"):
         analyzed=''
         for char in textvar:
             analyzed=analyzed+char.upper()
-        params = {'purpose':'After capitalisation', 'result': analyzed}
-        return render(request, 'analyze.html', params)
+        textvar=analyzed
+        # params = {'purpose':'After capitalisation', 'result': analyzed}
+        # return render(request, 'analyze.html', params)
     
-    elif(ana == "5"):
+    if(ana5 == "1"):
         analyzed=''
         for char in textvar:
             analyzed=analyzed+char.lower()
-        params = {'purpose':'Changed to lowercase', 'result': analyzed}
-        return render(request, 'analyze.html', params)
+        textvar=analyzed
+        # params = {'purpose':'Changed to lowercase', 'result': analyzed}
+        # return render(request, 'analyze.html', params)
 
-    elif(ana == "6"):
+    if(ana6 == "1"):
         analyzed = ''
         for char in textvar:
             if (char != '\n' and char != '\r'):
                 analyzed=analyzed+char
             else:
                 analyzed=analyzed+' '
-        params = {'purpose':'Merged in one line', 'result': analyzed}
-        return render(request, 'analyze.html', params)
+        textvar=analyzed
+        # params = {'purpose':'Final result is:', 'result': analyzed, 'count': counted}
+        # return render(request, 'analyze.html', params)
 
-    else:
+    if(ana1==ana2==ana3==ana4==ana5==ana6==0):
         return HttpResponse("Error")
+    
+    params = {'purpose':'Final result is:', 'result': textvar, 'count': counted}
+    return render(request, 'analyze.html', params)
